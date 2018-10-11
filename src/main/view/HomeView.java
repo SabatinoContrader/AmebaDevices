e2c9b6c75d2fe78a19a64deb6bb5589b98d266bf
raffcontrader <r.dellaporta@contrader.it>
2018-10-11 16:18:13 +0200

    delete old code

--- src/main/view/HomeView.java
+++ src/main/view/HomeView.java
@@ -2,7 +2,6 @@ package main.view;
 
 import main.MainDispatcher;
 import main.controller.Request;
-//import sun.applet.Main;
 
 import java.util.Scanner;
 
@@ -36,17 +35,6 @@ public class HomeView implements View {
         	System.out.println("3) Logout");
         }
         this.choice = Integer.parseInt(getInput());
-        /*if(userRole.equals("SUPERUSER")) {
-        	System.out.println("1) Inserisci customer");
-        	System.out.println("2) Visualizza customers");
-        	System.out.println("3) Modifica customer");
-        	System.out.println("4) Elimina customer");
-        }else {
-        	System.out.println("1) Inserisci building");
-        	System.out.println("2) Visualizza buildings");
-        	System.out.println("3) Modifica building");
-        	System.out.println("4) Elimina building");
-        }*/
     }
 
     public void submit() {
@@ -74,62 +62,8 @@ public class HomeView implements View {
     		}
     		else if (choice == 2) {
                 request.put("mode", "ItemMenu");
-                //MainDispatcher.getInstance().callAction("Customer", "doControl", request);
     		}
     	}
-    	/*if(userRole.equals("SUPERUSER")) {
-    		System.out.println("okok");
-        if (choice < 1 || choice > 5) 
-                MainDispatcher.getInstance().callAction("Home", "doControl", request);
-        else if (choice == 5)
-            MainDispatcher.getInstance().callAction("Login", "doControl", null);
-        else if(choice==1) {
-        	Request request = new Request();
-            request.put("mode", "InsertForm");
-            
-        	 MainDispatcher.getInstance().callAction("Customer", "doControl", request);
-        }
-        else if(choice==2) {
-        	Request request= new Request();
-        	request.put("mode", "Read");
-        	MainDispatcher.getInstance().callAction("Customer", "doControl", request);
-        }
-        else if(choice==3) {
-        	Request request = new Request();
-        	request.put("mode","UpdateForm");
-        	MainDispatcher.getInstance().callAction("Customer", "doControl", request);
-        }
-        else if(choice==4) {
-        	Request request= new Request();
-        	request.put("mode", "DeleteForm");
-        	MainDispatcher.getInstance().callAction("Customer", "doControl", request);
-        }
-    	}else {
-            switch (choice) {
-            case 1:
-            case 2: 
-            case 3: 
-            case 4:
-            	// Viene richiamato il BuildingController per le operazioni CRUD
-            	Request newRequest = new Request();
-            	newRequest.put("choice", choice);
-            	String prova=request.get("nomeUtente").toString();
-            	System.out.println(prova);
-            	newRequest.put("username", request.get("nomeUtente").toString());
-            	System.out.println("username="+ request.get("nomeUtente").toString());
-            	System.out.println("sono nel caso 4");
-            	System.out.println("nomeUtente="+request.get("nomeUtente").toString());
-            	
-            	MainDispatcher.getInstance().callAction("Building", "doControl", newRequest);
-            	break;
-            case 5:
-            	 MainDispatcher.getInstance().callAction("Login", "doControl", null);
-            	break;
-            default:
-            	 MainDispatcher.getInstance().callAction("Home", "doControl", request);
-            	break; 
-            }
-    	}*/
     }
 
 
