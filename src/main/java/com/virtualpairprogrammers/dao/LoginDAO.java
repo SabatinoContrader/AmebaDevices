745be3d88942fa689dc91e9008046ef72cb88f56
raffcontrader <r.dellaporta@contrader.it>
2018-10-17 12:51:50 +0200

    fix error

--- src/main/java/com/virtualpairprogrammers/dao/LoginDAO.java
+++ src/main/java/com/virtualpairprogrammers/dao/LoginDAO.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package com.virtualpairprogrammers.dao;
 
 import java.sql.Connection;
@@ -32,38 +31,3 @@ public class LoginDAO {
         return 0;
     }
 }
-=======
-package com.virtualpairprogrammers.dao;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import com.virtualpairprogrammers.utils.ConnectionSingleton;
-import com.virtualpairprogrammers.utils.GestoreEccezioni;
-
-public class LoginDAO {
-
-    private final String QUERY_LOGIN_CUSTOMER = "select * from user where username = ? and password = ?";
-    private final String QUERY_LOGIN_SUPERUSER = "select * from superuser where username = ? and password = ?";
-    
-    public boolean login (String username, String password, int choice) {
-    	PreparedStatement statement=null;
-        Connection connection = ConnectionSingleton.getInstance();
-        try {
-        	if(choice==1) {
-             statement = connection.prepareStatement(QUERY_LOGIN_SUPERUSER);
-            }else if(choice==2) {
-             statement = connection.prepareStatement(QUERY_LOGIN_CUSTOMER);
-            }
-            statement.setString(1, username);
-            statement.setString(2, password);
-            return statement.executeQuery().next();
-        }
-        catch (SQLException e) {
-            GestoreEccezioni.getInstance().gestisciEccezione(e);
-            return false;
-        }
-    }
-}
->>>>>>> Stashed changes
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -12,20 +12,3 @@
    </div>
 </body>
 <html>
-<<<<<<< Updated upstream
-=======
-<head>
-<title>Login</title>
-</head>
-<body>
-   <div>
-     <form action = "CustomerLogin" method= "post">
-     <h3>username: <input type = "text" id = "user" name ="username" placeholder = "inserisci username"></h3>
-     <h3>password: <input type = "password" id = "pass" name ="password" placeholder = "inserisci password"></h3>
-     <button type = "submit" value = "Login" name = "pulsante">Login In</button><br>
-     </form>
-     <a href="InsertCustomer?mode=InsertForm"> Registrati </a>
-   </div>
-</body>
-</html>
->>>>>>> Stashed changes
