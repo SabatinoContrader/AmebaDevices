c5ae250719f9401b1af1e6a140ef015cc30ba4c7
pietrocf02 <pcofone@contrader.it>
2018-11-05 11:45:58 +0100

    customer angular starting

---
+++ src/components/customer/customer.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+import { CustomerService } from "src/services/customer.service";
+
+@Component({
+    selector: 'app-customer',
+    templateUrl: './customer.component.html',
+    styleUrls: ['./customer.component.css']
+  })
+  export class SuperuserComponent implements OnInit{
+    constructor(private customerService: CustomerService ){}
+    ngOnInit(){}
+  }
+  
