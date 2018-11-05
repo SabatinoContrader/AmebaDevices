05b8b8389799be79e57127c54e65f15d12164964
martycontrader <m.muto@contrader.it>
2018-11-05 10:07:36 +0100

    Login fixed

--- src/main/java/com/AmebaDevices/controller/Login.java
+++ src/main/java/com/AmebaDevices/controller/Login.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import com.AmebaDevices.services.BuildingService;
 import com.AmebaDevices.services.CustomerService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/user")
 public class Login {
 
@@ -28,7 +30,7 @@ public class Login {
 		customerService = cs;
 		buildingService = bs;
 	}
-
+	@CrossOrigin
 	@RequestMapping(value="/login", method= RequestMethod.POST)
 	public CustomerDTO login
 	(
