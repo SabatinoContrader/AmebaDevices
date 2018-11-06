64a3071534fe8f015f1d925c492b4b24fe1a1f95
rinaldocontrader <r.rossi@contrader.it>
2018-11-06 10:28:55 +0100

    add crossorigin

--- src/main/java/com/AmebaDevices/controller/BuildingController.java
+++ src/main/java/com/AmebaDevices/controller/BuildingController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import com.AmebaDevices.services.CustomerService;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/Building")
 public class BuildingController {
 
