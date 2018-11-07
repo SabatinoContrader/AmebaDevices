91f38c3290a07508396993ed96b4b788320c8d8e
rinaldocontrader <r.rossi@contrader.it>
2018-11-07 16:22:37 +0100

    add crossOrigin

--- src/main/java/com/AmebaDevices/controller/ThingItemsController.java
+++ src/main/java/com/AmebaDevices/controller/ThingItemsController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +25,7 @@ import com.AmebaDevices.services.ThingService;
 import com.AmebaDevices.utils.SolutionGenerator;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/buildingTree")
 public class ThingItemsController {
 
